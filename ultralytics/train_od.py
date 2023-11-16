5d97367970bb6aee19885608ae2e01a48989d6c4
syz247179876 <247179876@qq.com>
2023-11-16 22:37:54 +0800

    update

--- ultralytics/train_od.py
+++ ultralytics/train_od.py
@@ -2,6 +2,4 @@ from ultralytics.models import YOLO
 
 if __name__ == '__main__':
     model = YOLO('yolov8n-ODConv.yaml')
-    model.train(data='VOC2.yaml', epochs=300, batch=32, lr0=0.015)
-    # results = model.train(data='coco128.yaml', epochs=3)
-    # model.val()
+    model.train(data='VOC2.yaml', epochs=300, batch=32, lr0=0.05)

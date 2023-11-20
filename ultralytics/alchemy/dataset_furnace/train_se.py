4fe5d9ae65c567c9a97f155954b58954eba11084
syz247179876 <247179876@qq.com>
2023-11-20 17:34:02 +0800

    update cfg of SE and SK

--- ultralytics/alchemy/dataset_furnace/train_se.py
+++ ultralytics/alchemy/dataset_furnace/train_se.py
@@ -7,4 +7,5 @@ from ultralytics.alchemy.settings import *
 
 if __name__ == '__main__':
     model = YOLO('yolov8n-SE-neck.yaml')
-    model.train(data=DATASET_EXDARK, epochs=300, batch=32, lr0=0.1, name=f'train-{DATASET_EXDARK}SE-neck')
+    model.train(data=DATASET_EXDARK, epochs=300, batch=32, lr0=0.1, name=f'train-{DATASET_EXDARK}SE-neck-pretrained',
+                pretrained='yolov8n.pt', freeze=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 16, 18, 20, 22, 24])
--- ultralytics/alchemy/dataset_furnace/train_sk.py
+++ ultralytics/alchemy/dataset_furnace/train_sk.py
@@ -7,4 +7,5 @@ from ultralytics.alchemy.settings import *
 
 if __name__ == '__main__':
     model = YOLO('yolov8n-SK-neck.yaml')
-    model.train(data=DATASET_EXDARK, epochs=300, batch=32, lr0=0.1, name=f'train-{DATASET_EXDARK}-SK-neck')
+    model.train(data=DATASET_EXDARK, epochs=300, batch=32, lr0=0.1, name=f'train-{DATASET_EXDARK}-SK-neck-pretrained',
+                pretrained='yolov8n.pt', freeze=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 18, 20, 22])
